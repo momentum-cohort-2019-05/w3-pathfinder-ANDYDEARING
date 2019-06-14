93c004e9f67ea2ecef6ffeb01f4cfb91aaffa51b
Andy Dearing <andy.dearing@gmail.com>
2019-06-14 16:24:07 -0400

    now calculates minimum and maximum values from the data files

--- pathfinder.py
+++ pathfinder.py
@@ -16,24 +16,45 @@ def read_file(file):
 class MapData:
     def __init__(self, data_file):
         self.data_file = data_file
+        self.max_value = self.get_max()
+        self.min_value = self.get_min()
     def get_width(self):
         return len(self.data_file)
     def get_length(self):
         return len(self.data_file[0])
+    def get_grayscale_value(self, x_coord, y_coord):
+        pass
+    def get_min(self):
+        list_of_mins = []
+        for row in self.data_file:
+            list_of_mins.append(min(row))
+        print(min(list_of_mins))
+        return min(list_of_mins)
+    def get_max(self):
+        list_of_maxes = []
+        for row in self.data_file:
+            list_of_maxes.append(max(row))
+        print(max(list_of_maxes))
+        return max(list_of_maxes)
 
 class MapImage:
     def __init__(self, map_data):
         self.map_data = map_data
-        self.image = Image.new('RGBA', (map_data.get_width(), map_data.get_length()) )
+        self.image = self.build_image()
     
     def show(self):
         self.image.show()
 
+    def build_image(self):
+        img = Image.new('RGBA', (map_data.get_width(), map_data.get_length()) )
+
+        return img
+
 # file = input("Which file shall I use? ")
-file = "elevation_small.txt"
+file = "elevation_large.txt"
 map_data = MapData(read_file(file))
 map_image = MapImage(map_data)
-map_image.show()
+# map_image.show()
 
 
 
