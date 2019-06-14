7e521a449af4aef24bf8cc61ac2c8d568e8b655c
Andy Dearing <andy.dearing@gmail.com>
2019-06-14 16:07:18 -0400

    now makes an empty map

--- pathfinder.py
+++ pathfinder.py
@@ -13,28 +13,30 @@ def read_file(file):
 
     return source_two_d_list
 
-file = input("Which file shall I use? ")
-two_d_elev_list = read_file(file)
+class MapData:
+    def __init__(self, data_file):
+        self.data_file = data_file
+    def get_width(self):
+        return len(self.data_file)
+    def get_length(self):
+        return len(self.data_file[0])
 
-print((two_d_elev_list))
-print(len(two_d_elev_list))
-print(len(two_d_elev_list[0]))
-
-# file  = "elevation_large.txt"
-# source_str_2d_list = []
-# attempt to store the image in a two dimensional list
-# with open(file) as source_file:
-#         source_str = source_file.read()
-
-# source_str = source_str.split("\n")
-
-# for line in source_str:
-#     if line.split() != []:
-#         source_str_2d_list.append([int(_) for _ in line.split()])
-
-# for list_of_str in source_str_2d_list:
-#     for elevation in list_of_str:
-#         elevation = int(elevation)
-
-
+class MapImage:
+    def __init__(self, map_data):
+        self.map_data = map_data
+        self.image = Image.new('RGBA', (map_data.get_width(), map_data.get_length()) )
     
+    def show(self):
+        self.image.show()
+
+# file = input("Which file shall I use? ")
+file = "elevation_small.txt"
+map_data = MapData(read_file(file))
+map_image = MapImage(map_data)
+map_image.show()
+
+
+
+# print((two_d_elev_list))
+# print(len(two_d_elev_list))
+# print(len(two_d_elev_list[0]))
